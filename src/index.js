a08bbb026a645309868306bd21e933a22c16e603
kali-user <hanesmitter3@gmail.com>
2020-09-02 00:43:39 +0300

    heroku app crash

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const unirest = require('unirest');
 
 const app = express();
 
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT || 3000;
 
 const publicDirPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirPath));
@@ -15,7 +15,6 @@ app.get('/pandemic', (req, res) => {
             error: "You must provide a contry name"
         })
     }
-    // res.send('yup! Gotten your message!');
     console.log(req.query.country);
     unirest
         .get(`https://covid-193.p.rapidapi.com/history?country=${req.query.country}`)
@@ -39,28 +38,3 @@ app.get('/pandemic', (req, res) => {
   app.listen(port, () => {
       console.log("server is running on port: " + port);
   });
-
- /*  var unirest = require("unirest");
-
-  var req = unirest("GET", "https://covid-193.p.rapidapi.com/statistics");
-  
-  req.headers({
-      "x-rapidapi-host": "covid-193.p.rapidapi.com",
-      "x-rapidapi-key": "0123a40dd6mshafcf1a330795890p1c96a9jsn8f46636f4f0b",
-      "useQueryString": true
-  });
-  
-  
-  req.end(function (res) {
-      if (res.error) throw new Error(res.error);
-  
-      console.log(res.body);
-  }); */
-  
-//documentation
-// Get the current status of all countries
-// get("https://covid-193.p.rapidapi.com/statistics");
-// Get the current status for a specific country
-// get("https://covid-193.p.rapidapi.com/statistics?country=china");
-// Get the current status in the world
-// get("https://covid-193.p.rapidapi.com/statistics?country=all");
